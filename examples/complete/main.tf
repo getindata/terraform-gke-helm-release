5868cb9c4fdc2e41673b9645940727c329a9a408
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2024-01-08 12:33:50 +0100

    test

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,6 +1,10 @@
 module "terraform_gke_helm_release" {
-  source     = "../../"
-  name       = "example"
-  namespace  = "default"
-  project_id = "example-project"
+  source        = "../../"
+  name          = "example-name"
+  namespace     = "default"
+  project_id    = "example-project"
+  chart         = "Chart.yaml"
+  repository    = "../repository"
+  chart_version = "0.1.0"
+  values        = "../repository/values.yaml"
 }
