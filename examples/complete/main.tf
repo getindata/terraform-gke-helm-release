d0107a914897ce3ab2be86405548c7abb7222622
Maciej PIetrzykowski <maciej.pietrzykowski@getindata.com>
2024-01-09 09:23:36 +0100

    add vars to complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,8 +1,11 @@
 module "terraform_gke_helm_release" {
-  source        = "../../"
-  name          = "example-name"
-  namespace     = "default"
-  project_id    = "example-project"
+  source     = "../../"
+  name       = "example-name"
+  namespace  = "default"
+  project_id = "example-project"
+  roles      = ["roles/storage.admin", "roles/compute.admin"]
+  additional_projects = { "my-gcp-project-name1" : ["roles/storage.admin", "roles/compute.admin"],
+  "my-gcp-project-name2" : ["roles/storage.admin", "roles/compute.admin"] }
   chart         = "app"
   repository    = "./repository"
   chart_version = "0.1.0"
