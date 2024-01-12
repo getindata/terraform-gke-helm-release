86deb0fe03ba87c329b669d4f43ccec1971dfbe4
MaciejMacQ <55737733+maciejmacq-dev@users.noreply.github.com>
2024-01-12 15:22:13 +0100

    New branch (#6)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,9 +1,26 @@
-module "terraform_module_template" {
-  source  = "../../"
-  context = module.this.context
+module "terraform_gke_helm_release" {
+  source               = "../../"
+  kubernetes_namespace = "default"
+  project_id           = "example-project"
+  name                 = "example-name"
+  descriptor_formats = {
+    gcp-service-account = {
+      labels = ["namespace", "environment", "name"]
+      format = "sa-%v-%v-%v"
+    }
+  }
+  values = [templatefile("./extra-values/values.yaml", {
+    replicaCount = 2
+  })]
+  service_account_value_path = "serviceAccount.name"
 
-  example_var = "This is a example value."
-  sub_resource = {
-    example_var = "This is a example value of sub resource."
+  app = {
+    name          = "example-name"
+    chart         = "nginx"
+    repository    = "https://charts.bitnami.com/bitnami"
+    version       = "15.6.1"
+    force_update  = true
+    wait          = false
+    recreate_pods = false
   }
 }
